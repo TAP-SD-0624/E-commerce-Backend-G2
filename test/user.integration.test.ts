d22edf3862fa65849130584cdebafe950b20834b
FerasAbushahla <abushahla21@gmail.com>
2024-08-21 16:23:44 +0300

    user register test done

--- test/user.integration.test.ts
+++ test/user.integration.test.ts
@@ -2,6 +2,8 @@ import sequelize from '../src/database/connection';
 import { shutdown, app, server } from '../src/server';
 import request from 'supertest';
 import { createUserDB } from '../src/utils/UsersUtils';
+import { createUser } from '../src/controllers/userController';
+import { log } from 'console';
 beforeAll(async () => {
     await sequelize
         .authenticate()
@@ -33,5 +35,35 @@ describe('integrate user and gest tasks', () => {
         console.log(resp.body.token);
 
         expect(resp.status).toEqual(201);
+    });
+});
+
+describe('register a user', () => {
+    const mockUser = {
+        user: {
+            firstName: 'feras',
+            lastName: 'samih',
+            email: 'samih@gmail.com',
+            phone: '88776656',
+            DOB: '1988-06-05T00:00:00.000Z',
+            imageUrl: 'bbb.jpg'
+        }
+    };
+    it('should create and register a user', async () => {
+        const response = await request(app).post('/user/register').send({
+            firstName: 'feras',
+            lastName: 'samih',
+            password: '11223344',
+            email: 'samih@gmail.com',
+            phone: '88776656',
+            DOB: '1988-06-05',
+            imageUrl: 'bbb.jpg'
+        });
+        console.log('-----------________(((((((()&&&&&^%');
+        console.log(response.body);
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('user');
+        expect(response.body).toHaveProperty('token');
     });
 });
